775b19f1069d01cfe5f04b229d85b8bcc53b376d
Alexander Sleat <alexander.sleat@uwe.ac.uk>
2018-06-19 19:39:17 +0100

    adding initial code base

---
+++ hearts_navigation/scripts/location_goal.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+
+# https://github.com/studioimaginaire/phue
+
+import rospy
+from geometry_msgs.msg import PoseStamped, Pose, Pose2D
+from std_msgs.msg import String
+import json
+import io
+import os
+
+
+# pose = Pose value
+# location = string value
+
+class Location():
+    def __init__(self):
+
+        self.PATH = rospy.get_param('locations_json')
+        # Load the location data
+        self.jsonCheck()
+        self.load_dict()
+        #self.write_dict()
+
+
+       
+        # Set up publishers
+        self.pubCurrent = rospy.Publisher('hearts/navigation/pose/location', String, queue_size=10)
+        self.pubGoal = rospy.Publisher('/hearts/navigation/goal', Pose2D, queue_size=10)
+
+
+        rospy.Subscriber("/record_Location", String, self.recordLocation_callback)
+        rospy.Subscriber("/go_To_Location", String, self.goToLocation_callback)
+
+        # rospy.Subscriber("/clicked_point", PointStamped, self.clicked_callback)
+        rospy.Subscriber("hearts/navigation/goal/location", String, self.locGoal_callback)
+        rospy.Subscriber("move_base_simple/current_pose", Pose, self.currentPose_callback)
+
+        self.loop()
+        self.current_location = ''
+        self.current_pose = Pose()
+        self.current_goal = ''
+
+        self.find_current_location()
+
+    def load_dict(self):
+        # todo: path as ros param
+        json_name = rospy.get_param('locations_json')
+
+        with open(json_name) as json_data:
+            self.dict = json.load(json_data)
+            
+        rospy.loginfo("using locations file: " + json_name)
+
+    def write_dict(self, updatedLocations):
+        # todo: path as ros param
+        json_name = rospy.get_param('locations_json')
+        with open(json_file, "w") as JSonDictFile:
+            json.dump(updatedLocations, json_file)
+
+
+    def jsonCheck(self):
+        if os.path.isfile(self.PATH) and os.access(self.PATH, os.R_OK):
+            # checks if file exists
+            print ("File exists and is readable")
+            return 1
+        else:
+            return 0
+
+
+    def recordLocation_callback(self, data):
+
+
+        p = {
+            data: 
+                {
+                "header": {
+                    "seq": 0,
+                    "stamp": {
+                        "secs": 0,
+                        "nsecs": 0
+                    },
+                    "frame_id": "/map"
+                },
+                "pose": {
+                    "position": {
+                        "x": self.current_pose.point.x,
+                        "y": self.current_pose.point.y,
+                        "z": 0
+                    },
+                    "orientation": {
+                        "x": 0,
+                        "y": 0,
+                        "z": 0,
+                        "w": 0
+                    }
+                }
+            }
+        }
+
+
+        if self.jsonCheck() == 1:
+            self.load_dict()
+            LocationsCurrent = self.dict
+            LocationsCurrent.update(p)
+        else:
+            with io.open(self.PATH, 'w') as db_file:
+                db_file.write(unicode(json.dumps({})))
+                LocationsCurrent = p
+        
+        self.write_dict(LocationsCurrent)
+
+
+    def goToLocation_callback(self, data):
+
+        goal_location = self.find_pose(data)
+        self.pubGoal.publish(pose)
+
+
+    # When a location is published, turn it in to a pose goal
+    def locGoal_callback(self, data):
+        print data.data
+        
+        pose = self.find_pose(data.data)
+        if not pose is None:
+            self.current_goal = data.data
+            self.pubGoal.publish(pose)
+        else:
+            self.current_goal = None
+            print "invalid goal location '" + data.data + "'"
+            
+    # When a pose is published, convert it to a location value
+    def currentPose_callback(self, data):
+        print data.data
+        self.current_pose = data.data
+        self.pubCurrent.publish(self.find_current_location())
+
+
+    # Get the pose values from the location dict
+    def find_pose(self, location_name):
+
+        try:
+            print self.dict[location_name]
+            goal = Pose2D()
+            goal.x = self.dict[location_name]["x"]
+            goal.y = self.dict[location_name]["y"]
+            goal.theta = self.dict[location_name]["theta"]
+
+            return goal
+        except:
+            return None
+
+
+
+    # Get the location value from the Pose lookup
+    def find_current_location(self):
+
+        # @TODO get current pose
+
+        currentPose = self.current_pose
+        location = ''
+        for i in self.dict:
+            if (self.dict[i]["x"] == currentPose.position.x and self.dict[i]["y"] == currentPose.position.y):
+                location = i
+
+        return location
+
+    # loop
+    def loop(self):
+
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node("task_controller", anonymous=True)
+    loc = Location()
+    rospy.spin()
---
+++ hearts_navigation/scripts/navigator.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped, Pose, Pose2D
+from actionlib_msgs.msg import GoalStatusArray, GoalID
+from std_msgs.msg import String
+from std_msgs.msg import UInt8
+#from roah_rsb_comm_ros import BenchmarkState
+
+class Navigator():
+
+    def __init__(self):
+
+        self.isNavigating = False
+        self.currentGoal = Pose2D()
+        self.lastGoal = Pose2D()
+
+        self.pubGoal = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+
+        self.pubStatus = rospy.Publisher('/hearts/navigation/status', String, queue_size=1)
+        self.pubPose = rospy.Publisher('/hearts/navigation/current', String, queue_size=1)
+
+        self.previous_state = ""
+        self.t = PoseStamped()
+        rospy.Subscriber('move_base/status', GoalStatusArray, self.StatusCallback)   # Get status of plan
+
+        rospy.Subscriber('hearts/navigation/goal', Pose2D, self.goalCallback)
+        rospy.Subscriber('hearts/navigation/stop', String, self.stopCallback)
+        
+    def goalCallback(self, data):
+
+        rospy.loginfo("Navigator: goal Callback")
+
+        # Make sure the robot isn't already trying to go somewhere
+        if(self.isNavigating == True):
+            rospy.loginfo("Navigator: currently navigating, can't continue at the moment.")
+        else:
+            rospy.loginfo("Navigator: sending new goal pose")
+
+            # @TODO add some form of checking to data:
+
+            self.currentGoal = data
+
+            # Convert Pose2D to PoseStamped
+            self.t = PoseStamped()
+            self.t.header.frame_id = "/map"
+            self.t.pose.position.x = data.x
+            self.t.pose.position.y = data.y
+            #t.pose.orientation.w = data.theta
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, data.theta)
+            self.t.pose.orientation.x = quaternion[0]
+            self.t.pose.orientation.y = quaternion[1]
+            self.t.pose.orientation.z = quaternion[2]
+            self.t.pose.orientation.w = quaternion[3]
+            # = createQuaternionMsgFromYaw(data.theta)
+            rospy.loginfo(str(data.x)+ str(data.y)+ str(data.theta))
+            self.pubGoal.publish(self.t)
+            self.isNavigating = True
+
+    def stopCallback(self, data):
+
+        if (data.data):
+            rospy.loginfo("Navigator: Stop Callback")
+
+    def StatusCallback(self, data):
+        length_status = len(data.status_list)
+
+        if length_status > 0:
+            status = data.status_list[length_status-1].status
+            
+            if status == 4 or status == 5 or status == 9:
+                status_msg = 'Fail'
+                self.isNavigating = False
+            elif status==3:
+                status_msg = 'Success'
+                self.lastGoal = self.currentGoal
+                self.currentGoal = Pose2D()
+                self.isNavigating = False
+                self.repeat = False
+            else:
+                status_msg = 'Active'
+                self.isNavigating = True
+
+            if self.previous_state != status_msg: 
+                self.pubStatus.publish(status_msg)
+                self.previous_state = status_msg
+                rospy.loginfo('status: ' + str(status))
+
+                    
+
+if __name__ == '__main__':
+	rospy.init_node('Navigator', anonymous=True)
+	rospy.loginfo("Navigator has started")
+	n = Navigator()
+	rospy.spin()
