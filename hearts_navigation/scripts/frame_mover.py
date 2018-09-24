4ef2cdd018d00cefd29e8d078844877c8eee2b5f
Richard Wakeling-Gentle <rw17681@bristol.ac.uk>
2018-09-24 15:40:41 +0100

    Add functions to print realative locations

--- hearts_navigation/scripts/frame_mover.py
+++ hearts_navigation/scripts/frame_mover.py
@@ -2,40 +2,86 @@
 import roslib
 import rospy
 import tf
+from geometry_msgs.msg import TransformStamped, Quaternion
 from std_msgs.msg import String
 
-'''Simple script to place a fixed TF frame called erl_frame on the ground at
-    Tiago's current location and orientation. Used for setting the origin of
-    the competition coordinate frame'''
+'''Deals with seting the locations for erl coordinate frame and target locaiton,
+   also returns tiago'location in erl coordinates and target's location relative
+   to tiago'''
 
-#The frame coords are updated whenever anything is published to the 
-#"set_erl_frame" topic
 
+#set erl_frame coords to be tiago's current location
 def frame_change(x):
     (pos,ori) = listener.lookupTransform("/odom","/base_footprint",rospy.Time())
     rospy.set_param('erl_pos', pos)
     rospy.set_param('erl_ori', ori)
 
+#sets target location relative to erl_frame
+def set_target(target):
+    pos = [target.transform.translation.x,
+           target.transform.translation.y,
+           target.transform.translation.z]
+    ori = [float(target.transform.rotation.x),
+           float(target.transform.rotation.y),
+           float(target.transform.rotation.z),
+           float(target.transform.rotation.w)] 
+    rospy.set_param('target_pos', pos)
+    rospy.set_param('target_ori', ori)
+
+#prints the goal's location relative to tiago
+def tiago_to_goal(x):
+    (pos,ori) = listener.lookupTransform("/base_footprint","/target",rospy.Time())
+    rospy.loginfo(pos)
+    rospy.loginfo(ori)
+
+#prints tiago's location relative to the erl_frame
+def tiago_loc(x):
+    (pos,ori) = listener.lookupTransform("/erl_frame","/base_footprint",rospy.Time())
+    rospy.loginfo(pos)
+    rospy.loginfo(ori)
+
+
 #create the node
 rospy.init_node('transform_mover')
-#broadcaster to let ros know where the frame is
+
+#broadcaster to let ros know where the frames are
 br = tf.TransformBroadcaster()
-#subscribe to the required topic
+
+#subscribe to the required topics
 rospy.Subscriber("set_erl_frame", String, frame_change)
+rospy.Subscriber("set_move_target", TransformStamped, set_target)
+rospy.Subscriber("where_is_target", String, tiago_to_goal)
+rospy.Subscriber("where_am_i", String, tiago_loc)
+
 #listener allows us to look up Tiago's current location
 listener = tf.TransformListener()
+
 #sleep to give everything time to initialize
 rospy.sleep(1)
-#invoke the frame changer so the frame exists when the while loop starts
-frame_change('x')
-#frame is published at 1Hz
-r = rospy.Rate(1)    
 
-#continuously publish the frame so that other tf functions will work
+#invoke frame change & set_target so the frames exists when the while loop starts
+frame_change('x')
+#create a transform of all 0 to set as initial target
+targ = TransformStamped()
+targ.transform.translation.x = 0
+targ.transform.translation.y = 0
+targ.transform.translation.z = 0
+targ.transform.rotation = Quaternion(0,0,0,1)
+
+set_target(targ)
+
+#frames are published at 1Hz
+r = rospy.Rate(1)
+
+#continuously publish the frames so that other tf functions can use them
 while not rospy.is_shutdown():
     pos = rospy.get_param('erl_pos')
     ori = rospy.get_param('erl_ori')
     br.sendTransform(pos,ori,rospy.Time.now(),"/erl_frame","/odom")
+
+    pos = rospy.get_param('target_pos')
+    ori = rospy.get_param('target_ori')
+    br.sendTransform(pos,ori,rospy.Time.now(),"/target","/erl_frame")
     r.sleep()
 
 #if __name__ == '__main__':    
